be534fe4f71ecea26226d698ae03c68f016affbc
Lakhdar Belkharroubi <83500639+lakhdar-blk@users.noreply.github.com>
2022-04-05 14:11:45 +0100

    Update settings.py

--- myportf/settings.py
+++ myportf/settings.py
@@ -140,7 +140,7 @@ DEFAULT_FROM_EMAIL = 'mnsfcb@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'mnsfcb@gmail.com'
-EMAIL_HOST_PASSWORD = 'HAMZA206KELTOUM206'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
